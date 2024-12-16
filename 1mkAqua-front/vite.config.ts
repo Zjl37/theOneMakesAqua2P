255f5460ae36f494f4b396db24b0109b4fed1f75
Zjl37 <2693911885@qq.com>
2024-12-17 00:08:49 +0800

    整理上传

---
+++ 1mkAqua-front/vite.config.ts
@@ -0,0 +1,59 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+
+import Vue from '@vitejs/plugin-vue'
+import VueRouter from 'unplugin-vue-router/vite'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+import Unocss from 'unocss/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "~/styles/element/index.scss" as *;`,
+        api: 'modern-compiler',
+      },
+    },
+  },
+
+  plugins: [
+    Vue(),
+
+    // https://github.com/posva/unplugin-vue-router
+    VueRouter({
+      extensions: ['.vue', '.md'],
+      dts: 'src/typed-router.d.ts',
+    }),
+    
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+
+    Components({
+      // allow auto load markdown components under `./src/components/`
+      extensions: ['vue', 'md'],
+      // allow auto import and register components used in markdown
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      resolvers: [
+        ElementPlusResolver({
+          importStyle: 'sass',
+        }),
+      ],
+      dts: 'src/components.d.ts',
+    }),
+
+    // https://github.com/antfu/unocss
+    // see uno.config.ts for config
+    Unocss(),
+  ],
+})
